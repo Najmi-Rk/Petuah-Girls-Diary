af35cc4726fe707f7040e7ba06824e991063c0ba
Najmi-Rk <najmi.rakhmapradyptaa@gmail.com>
2025-05-28 21:13:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Petuah Girls Diary (*^ー^)ノ♪")
+st.title("Petuah Girls Diary  (*^ー^)ノ♪")
 st.write("Let's keep our memories gurl")
 
 st.video("VID-20240304-WA0019.mp4")
