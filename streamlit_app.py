45313ad79568c9d6d32324db03a6f87d5dc69ce0
Najmi-Rk <najmi.rakhmapradyptaa@gmail.com>
2025-05-28 21:22:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ st.image("IMG-20240223-WA0007.jpg")
 st.write("hahaha foto pertama kitaa best")
 
 
+st.image("IMG-20240304-WA0017.jpg")
+siapa yang kangen petuah disini?
+
+
+st.image("")
+st.image("")
+st.image("")
