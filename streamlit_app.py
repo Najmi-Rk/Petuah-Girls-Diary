1af064cecf9d492e5cac5f74ca376bcfbba78e7a
Najmi-Rk <najmi.rakhmapradyptaa@gmail.com>
2025-05-28 21:41:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Petuah Girls Diary  (*^ー^)ノ♪")
+st.title("Petuah Girls Diary (*^ー^)ノ♪")
 st.write("Let's keep our memories gurl")
 st.write("♡")
 st.image("IMG-20240304-WA0017.jpg")
